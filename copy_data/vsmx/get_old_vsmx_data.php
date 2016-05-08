b49a65a31cb57ca4878b9866495c92f9511e963f
Veselin VACHEV <veselin.vachev@gmail.com>
2016-05-08 21:00:48 +0200

    aa

--- copy_data/vsmx/get_old_vsmx_data.php
+++ copy_data/vsmx/get_old_vsmx_data.php
@@ -18,23 +18,6 @@ $inserted_query = 0;
 // DATES
 // UNIX timestamps
 
-if(isset($_GET['rattrappe_retard']))
-{
-  $journee = getdate((time()-86400));
-}
-else
-{
-  $journee = getdate(time());
-  $journee= $journee['year']."-".$journee['mon']."-".$journee['mday'];
-}
-
-
-if(isset($_GET['journee']))
-{
-  $journee = $_GET['journee'];
-}
-
-
 //etablissement connection PDO with C170 pour le tracking des journées
 try {$db_vesnet= new PDO("mysql:host=10.10.10.111;dbname=vesnet", 'root', 'vesnet@home');}
 catch(PDOException $e){echo getcwd(); echo $e->getMessage();}
@@ -42,22 +25,22 @@ catch(PDOException $e){echo getcwd(); echo $e->getMessage();}
 $query = " SELECT DISTINCT `date` FROM `track_data_copy` WHERE `nb_cdr_inserted`> 0 AND `time_needed` > 0 ORDER BY `date` ASC LIMIT 1;";
 $res_req=$db_vesnet->query($query);
 $smaller_date=$res_req->fetchAll(PDO::FETCH_BOTH);
-$smaller_date = $smaller_date[0][0];
+if(empty($smaller_date))
+  {
+    $journee = getdate(time());
+    $journee= $journee['year']."-".$journee['mon']."-".$journee['mday'] ;
+  }
+else
+  {
+    $smaller_date = $smaller_date[0][0];
+    $journee = date_create("$smaller_date");
+    date_sub($journee, date_interval_create_from_date_string('1 day'));
+    $journee= date_format($journee, 'Y-m-d');
+  }
 
+$content=file_get_contents("http://10.10.10.111/CDRstore/copy_data/vsmx/get_calls_vsmx_v3.php?journee=$journee");
 
-$journee = date_create("$smaller_date");
-date_sub($journee, date_interval_create_from_date_string('1 day'));
-$journee= date_format($journee, 'Y-m-d');
-
-
-
-
-
-
-
-//$content=file_get_contents("https://91.230.169.73/st/liste-dev/extra_power/drop_cdr_temp_and_cdr_instant.php");
-
-//echo "Get Latest CSVs : "; echo  $content;
+echo "Get Latest CSVs : "; echo  $content;
 
 
 
